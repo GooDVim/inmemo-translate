a7e38f7051d218514645795d6056af77ce7ac323
Dmitry Gorshkov <dmitry.gorshkov@futurice.com>
2016-08-29 23:01:36 +0300

    Get rid of wierd parts of speech

--- src/Inmemo.Translate/Models/PartOfSpeech.cs
+++ src/Inmemo.Translate/Models/PartOfSpeech.cs
@@ -2,19 +2,16 @@ namespace Inmemo.Translate.Models
 {
     public enum PartOfSpeech
     {
-        Noun,
-        Verb,
-        Adjective,
-        Adverb,
-        Article,
-        Conjunction,
-        Demonstrative,
-        Existential,
-        Preposition,
-        Number,
-        Pronoun,
-        InvinitiveMarker,
-        Interjection,
-        Negotion
+        Noun = 0,
+        Verb = 1,
+        Adjective = 2,
+        Adverb = 3,
+        Preposition = 4,
+        Pronoun = 5,
+        Number = 6,
+        Conjunction = 7,
+        Interjection = 8,
+        Article = 9,
+        Negotion = 10
     }
 }
